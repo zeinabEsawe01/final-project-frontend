8849a90389e89de2db75b77d5852a9bdcf09892f
Zeinab Esawe <zeinab.esawe@gmail.com>
2023-04-18 18:07:32 +0300

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 
 import SignUpForm from './Components/SignUpForm/SignUpForm';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router,  Route, Routes } from 'react-router-dom';
+import Login from './Components/loginForm/Login';
 import Login from './Components/login/Login';
 import Navbar from './Components/Navbar/Navbar';
 
 const App = () => {
   
   return (
+    
     <Router>
-    <div>
-      <Navbar/>
-    <SignUpForm />
-    <Login/>
-    </div>
-    <Routes>
-      <Route path='/'/>
-    </Routes>
+      <div>
+        <Navbar/>
+      </div>
+        <Routes>
+        <Route path="/" element={<SignUpForm/>} />
+        <Route path="/login" element={<Login/>}/>
+
+        </Routes>
     </Router>
   );
 }
