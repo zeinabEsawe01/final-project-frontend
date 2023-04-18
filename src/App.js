2a1a2ed03870015edc3e78ec398d0bad308be9e4
Zeinab Esawe <zeinab.esawe@gmail.com>
2023-04-18 17:50:27 +0300

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 
 import SignUpForm from './Components/SignUpForm/SignUpForm';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/login/Login';
+import Navbar from './Components/Navbar/Navbar';
 
 const App = () => {
   
   return (
+    <Router>
     <div>
+      <Navbar/>
     <SignUpForm />
     <Login/>
     </div>
+    <Routes>
+      <Route path='/'/>
+    </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import { FaUserCircle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { NavDropdown } from 'react-bootstrap';
+
+
+export default function navbar() {
+      return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">My App</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            <NavDropdown title={<FaUserCircle className="profile-icon" size={30} />} id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/1">Reset your password</NavDropdown.Item>
+              <NavDropdown.Item href="#action/2">Log Out</NavDropdown.Item>
+            </NavDropdown>
+          </ul>
+        </div>
+      </nav>
+    );
+}
