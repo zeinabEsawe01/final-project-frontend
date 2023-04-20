ce66b93e8d1637aad8018873205886edd5b20265
Zeinab Esawe <zeinab.esawe@gmail.com>
2023-04-20 14:21:15 +0300

    pull for changing

--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -24,7 +24,7 @@ const SignUpForm = () => {
     }
     const [errors, setErrors] = useState({});
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const newErrors = {};
 
